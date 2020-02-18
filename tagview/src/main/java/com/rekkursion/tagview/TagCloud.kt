daa87c361d5019cad6c1e1c6f9fddf357110373d
Rekkursion <rekkursion233@gmail.com>
2020-02-18 14:47:44 +0800

    Add add-tag button inside the tag-cloud as its feature

--- tagview/src/main/java/com/rekkursion/tagview/TagCloud.kt
+++ tagview/src/main/java/com/rekkursion/tagview/TagCloud.kt
@@ -1,15 +1,22 @@
 package com.rekkursion.tagview
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.ImageButton
 import androidx.core.view.children
 import com.google.android.flexbox.FlexboxLayout
 
 class TagCloud(context: Context, attrs: AttributeSet? = null): FrameLayout(context, attrs) {
     // for placing all tag-views
     private val mFblTagsContainer: FlexboxLayout
+
+    // for adding new tag-view
+    private val mImgbtnAddNewTag: ImageButton
 
     // for storing the already-exists tag-views
     private val mTagStringsHashMap: HashMap<String, TagView> = hashMapOf()
@@ -41,6 +48,39 @@ class TagCloud(context: Context, attrs: AttributeSet? = null): FrameLayout(conte
 
         // get views
         mFblTagsContainer = findViewById(R.id.fbl_tags_container)
+        mImgbtnAddNewTag = findViewById(R.id.imgbtn_add_new_tag)
+
+        // set events
+        mImgbtnAddNewTag.setOnClickListener {
+            // the edit-text for the user enters the tag string
+            val edtNewTagString = EditText(context)
+            edtNewTagString.requestFocus()
+
+            // the dialog shows for typing
+            val dialog = AlertDialog.Builder(context)
+                .setTitle("Enter the tag")
+                .setView(edtNewTagString)
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Cancel", null)
+                .create()
+            dialog.setOnShowListener {
+                val btnPositive = (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE)
+                btnPositive.setOnClickListener {
+                    if (doesStringAlreadyExists(edtNewTagString.text.toString())) {
+                        AlertDialog.Builder(context)
+                            .setMessage("The tag already exists")
+                            .setPositiveButton("OK", null)
+                            .create()
+                            .show()
+                    }
+                    else {
+                        addTag(edtNewTagString.text.toString())
+                        dialog.dismiss()
+                    }
+                }
+            }
+            dialog.show()
+        }
     }
 
     /* =================================================================== */
@@ -76,8 +116,8 @@ class TagCloud(context: Context, attrs: AttributeSet? = null): FrameLayout(conte
         // no problem
         else {
             // add the created tag-view into the container
-            if (index == null) mFblTagsContainer.addView(tagView)
-            else mFblTagsContainer.addView(tagView, index)
+            if (index == null) mFblTagsContainer.addView(tagView, mFblTagsContainer.childCount - 1)
+            else mFblTagsContainer.addView(tagView, if (index >= mFblTagsContainer.childCount) mFblTagsContainer.childCount - 1 else index)
 
             // add the string of new tag into the hash-set
             mTagStringsHashMap[tagString] = tagView
@@ -86,12 +126,12 @@ class TagCloud(context: Context, attrs: AttributeSet? = null): FrameLayout(conte
 
     // remove a certain tag-view by index
     fun removeTagByIndex(index: Int): Boolean {
-        if (index < 0 || index >= mFblTagsContainer.childCount)
+        if (index < 0 || index >= mFblTagsContainer.childCount - 1)
             return false
 
         val tagView = (mFblTagsContainer.getChildAt(index) as? TagView) ?: return false
         mFblTagsContainer.removeView(tagView)
-        mOnTagRemoveListener?.onTagRemove(this@TagCloud, tagView, index, mFblTagsContainer.childCount)
+        mOnTagRemoveListener?.onTagRemove(this@TagCloud, tagView, index, mFblTagsContainer.childCount - 1)
         mTagStringsHashMap.remove(tagView.tagString)
 
         return true
@@ -109,11 +149,23 @@ class TagCloud(context: Context, attrs: AttributeSet? = null): FrameLayout(conte
 
     /** getters */
 
+    // get the number of tags
+    fun getNumOfTags(): Int = mFblTagsContainer.childCount - 1
+
+    // get all tag-views
+    fun getAllTags(): List<TagView> = mFblTagsContainer.children.filter { it is TagView }.map { it as TagView }.toList()
+
+    // get tag strings of all tag-views
+    fun getAllTagStrings(): List<String> = mFblTagsContainer.children.filter { it is TagView }.map { (it as TagView).tagString }.toList()
+
     // get the tag-view by index
-    fun getTagAt(index: Int): TagView = mFblTagsContainer.getChildAt(index) as TagView
+    fun getTagAt(index: Int): TagView? = mFblTagsContainer.getChildAt(index) as? TagView
 
     // get the tag string of a tag-view by index
-    fun getTagStringAt(index: Int): String = getTagAt(index).tagString
+    fun getTagStringAt(index: Int): String? = getTagAt(index)?.tagString
+
+    // check if a string is conflicted w/ the already-exists tags
+    fun doesStringAlreadyExists(str: String): Boolean = mTagStringsHashMap.containsKey(str)
 
     /* =================================================================== */
 
